833eaf506cf35481b9421ae7d9de24067e4c3ce7
Gabriel <gchiriac100@gmail.com>
2024-12-17 02:08:27 +0200

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # WebProjectMovies
+
+version 1
---
+++ app.js
@@ -0,0 +1,5 @@
+const arrow = document.querySelector(".arrow");
+console.log(arrow)
+const movieLists = document.querySelectorAll(".movielists")
+console.log(movieLists)
+console.log("Gabriel");
